ea4cf6245fadc3dcf13872655632bda1cee531ec
Paul Winter <pwinter001@gmail.com>
2018-08-17 21:33:54 -0700

    Add Display Method

--- lib/GameBoard.rb
+++ lib/GameBoard.rb
@@ -22,6 +22,34 @@ class GameBoard
 			return nil
 		end
 	end
+
+	def display
+		row = 7
+		column = 0
+		top = "┏━━━━━━━━━━━━━━━━━┓\n"
+		bottom = "┗━━━━━━━━━━━━━━━━━┛\n" + "+ 1 2 3 4 5 6 7 8 +\n"
+		rows = ""
+		while row >= 0
+			rows << "┃ "
+			while column < 8
+				if square_at([row, column]).occupant
+					symbol = square_at([row, column]).occupant.symbol
+				elsif (row % 2 == 0 && column % 2 == 0) || 
+					  (row % 2 == 1 && column % 2 == 1)
+					    symbol = '⬛'
+				else
+					symbol = '⬜'
+				end
+				rows << symbol + " "
+				column += 1
+			end
+			rows << "┃\n"
+			row -= 1
+			column = 0
+		end
+		board = top + rows + bottom
+		puts board
+	end
 end
 
 class ChessSquare
