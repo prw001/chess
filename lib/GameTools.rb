946c8891549329709d42dfbf636114f9cb5268a3
Paul Winter <pwinter001@gmail.com>
2018-08-17 21:31:02 -0700

    recreate file to be board constructer

--- lib/GameTools.rb
+++ lib/GameTools.rb
@@ -1,101 +1,47 @@
-module BoardBuilder
+require './GameBoard.rb'
+require './GamePiece.rb'
 
-end
+module GameTools
+	def create
+		game = GameBoard.new
+		black_pieces = [
+			Rook.new(game, game.square_at([0, 0]), 'b'),
+			Knight.new(game, game.square_at([0, 1]), 'b'),
+			Bishop.new(game, game.square_at([0, 2]), 'b'),
+			Queen.new(game, game.square_at([0, 3]), 'b'),
+			King.new(game, game.square_at([0, 4]), 'b'),
+			Bishop.new(game, game.square_at([0, 5]), 'b'),
+			Knight.new(game, game.square_at([0, 6]), 'b'),
+			Rook.new(game, game.square_at([0, 7]), 'b'),
+		]
 
-module PieceMoves
-
-	def get_next_coordinates(origin, direction)
-		next_coordinates = origin.dup
-		case direction
-		when 'N'
-			next_coordinates[0] -= 1
-		when 'NE'
-			next_coordinates[1] += 1
-			next_coordinates[0] -= 1
-		when 'E'
-			next_coordinates[1] += 1
-		when 'SE'
-			next_coordinates[1] += 1
-			next_coordinates[0] += 1
-		when 'S'
-			next_coordinates[0] += 1
-		when 'SW'
-			next_coordinates[1] -= 1
-			next_coordinates[0] += 1
-		when 'W'
-			next_coordinates[1] -= 1
-		when 'NW'
-			next_coordinates[1] -= 1
-			next_coordinates[0] -= 1
-		else
-			next_coordinates = nil
+		8.times do |index|
+			black_pieces << Pawn.new(game, game.square_at([1, index]), 'b')
 		end
 
-		return next_coordinates
-	end
-
-	def get_pawn_moves(board, pawn)
-		moveset = []
-		origin = pawn.position.coordinates
-		next_coordinates = origin.dup
-
-		pawn.color == 'w' ? directions = ['N', 'NE', 'NW'] : directions = ['S', 'SE', 'SW'] 
-		pawn.first_move == true ? move_delta = 2 : move_delta = 1 
-		
-		move_delta.times do #vertical moves
-			next_coordinates = get_next_coordinates(next_coordinates, directions[0])
-			next_square = board.square_at(next_coordinates)
-			if next_square && (next_square.occupant == nil)
-				moveset << next_square
-			else
-				break
-			end
+		black_pieces.each do |piece|
+			piece.position.occupant = piece
 		end
 
-		2.times do |index| #diagonal attacks
-			next_square = board.square_at(get_next_coordinates(origin, directions[index + 1]))
-			if next_square && next_square.occupant && next_square.occupant.color != pawn.color
-				moveset << next_square
-			end
+		white_pieces = [
+			Rook.new(game, game.square_at([7, 0]), 'w'),
+			Knight.new(game, game.square_at([7, 1]), 'w'),
+			Bishop.new(game, game.square_at([7, 2]), 'w'),
+			Queen.new(game, game.square_at([7, 3]), 'w'),
+			King.new(game, game.square_at([7, 4]), 'w'),
+			Bishop.new(game, game.square_at([7, 5]), 'w'),
+			Knight.new(game, game.square_at([7, 6]), 'w'),
+			Rook.new(game, game.square_at([7, 7]), 'w'),
+		]
+
+		8.times do |index|
+			white_pieces << Pawn.new(game, game.square_at([6, index]), 'w')
 		end
 
-		return moveset
-	end
-
-	def get_rook_moves(board, rook)
-
-	end
-
-	def get_knight_moves(board, knight)
-
-	end
-
-	def get_bishop_moves(board, bishop)
-
-	end
-
-	def get_queen_moves(board, queen)
-
-	end
-
-	def get_king_moves(board, king)
-
-	end
-
-	def get_legal_moves(board, piece)
-		case true
-		when (piece.instance_of? Pawn)
-			return get_pawn_moves(board, piece)
-		when (piece.instance_of? Rook)
-			return get_rook_moves(board, piece)
-		when (piece.instance_of? Knight)
-			return get_knight_moves(board, piece)
-		when (piece.instance_of? Bishop)
-			return get_bishop_moves(board, piece)
-		when (piece.instance_of? Queen)
-			return get_queen_moves(board, piece)
-		else
-			return get_king_moves(board, piece)
+		white_pieces.each do |piece|
+			piece.position.occupant = piece
 		end
+
+		return game
 	end
 end
