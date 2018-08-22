02e7a6f567b523a6caf4c0644c8072b97e0b7534
Paul Winter <pwinter001@gmail.com>
2018-08-21 21:52:12 -0700

    Change display method, factor out board colorization to separate method

--- lib/GameBoard.rb
+++ lib/GameBoard.rb
@@ -1,7 +1,10 @@
+require 'colored2'
 class GameBoard
-	attr_reader :rows
 	attr_accessor :black_pieces
 	attr_accessor :white_pieces
+	attr_accessor :game_over
+	attr_reader :player_turn
+	attr_reader :rows
 	def create_rows
 		rows = []
 		8.times do |row_index| 
@@ -13,7 +16,7 @@ class GameBoard
 		return rows
 	end
 
-	def initialize(black_pieces = nil, white_pieces = nil, rows = nil)
+	def initialize(black_pieces = nil, white_pieces = nil, rows = nil, player_turn = 1)
 		unless rows
 			@rows = create_rows
 		else
@@ -21,6 +24,16 @@ class GameBoard
 		end
 		@black_pieces = black_pieces
 		@white_pieces = white_pieces
+		@player_turn = player_turn
+		@game_over = false
+	end
+
+	def turn_over
+		if @player_turn == 1
+			@player_turn = 2
+		else
+			@player_turn = 1
+		end
 	end
 
 	def square_at(coordinates)
@@ -32,27 +45,46 @@ class GameBoard
 		end
 	end
 
-	def display
+	def color(text, row = nil, column = nil)
+		if row && column
+			if (row % 2 == 0 && column % 2 == 0) || 
+			   (row % 2 == 1 && column % 2 == 1)
+			   		return text.on.blue
+			else
+				return text.on.magenta
+			end
+		else
+			return text.on.green
+		end
+	end
+
+	def display_with_options(moveset = nil)
+		row_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 		row = 7
 		column = 0
-		top = "┏━━━━━━━━━━━━━━━━━┓\n"
-		bottom = "┗━━━━━━━━━━━━━━━━━┛\n" + "+ 1 2 3 4 5 6 7 8 +\n"
+		top = "  ┏━━━━━━━━━━━━━━━━━━┓\n"
+		bottom = "  ┗━━━━━━━━━━━━━━━━━━┛\n" + "  + 1 2 3 4 5 6 7 8 +\n"
 		rows = ""
 		while row >= 0
-			rows << "┃ "
+			rows << "#{row_labels[row]} ┃ "
 			while column < 8
-				if square_at([row, column]).occupant
-					symbol = square_at([row, column]).occupant.symbol
-				elsif (row % 2 == 0 && column % 2 == 0) || 
-					  (row % 2 == 1 && column % 2 == 1)
-					    symbol = '⬛'
+				unless moveset && (moveset.include? square_at([row, column]))
+					if square_at([row, column]).occupant
+						symbol = color(square_at([row, column]).occupant.symbol, row, column)
+					else
+					    symbol = color('  ', row, column)
+					end
 				else
-					symbol = '⬜'
+					if square_at([row, column]).occupant
+						symbol = color(square_at([row, column]).occupant.symbol)
+					else
+						symbol = color('  ')
+					end
 				end
-				rows << symbol + " "
+				rows << symbol
 				column += 1
 			end
-			rows << "┃\n"
+			rows << " ┃\n"
 			row -= 1
 			column = 0
 		end
