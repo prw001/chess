bc07fa1060cdcf70c09c628ea4b0c26ceaa58b8d
Paul Winter <pwinter001@gmail.com>
2018-08-21 21:56:59 -0700

    migrate .checkmate test to this suite

--- spec/GameTools_spec.rb
+++ spec/GameTools_spec.rb
@@ -9,7 +9,7 @@ describe "GameTools" do
 			it 'copies that array, but not its location in memory' do 
 				a = [[1, 2], [3]]
 				b = dummy_class.deep_copy(a)
-				
+
 				expect(a.equal? b).to eql(false)
 				expect(a == b).to eql(true)
 			end
@@ -52,6 +52,56 @@ describe "GameTools" do
 				game.white_pieces = ['-', '-', '-', '-', king]
 
 				expect(dummy_class.puts_king_in_check(game, pawn, destination)).to eql(false)
+			end
+		end
+	end
+
+	describe "#checkmate" do 
+
+		context "when the King is in check" do 
+
+			context "when the King cannot move anywhere else" do 
+				game = GameBoard.new
+				rook_sq_one = game.square_at([0, 7])
+				rook_sq_two = game.square_at([1, 6])
+				king_square = game.square_at([7, 7])
+				rook_one = Rook.new(game, rook_sq_one, 'w')
+				rook_sq_one.occupant = rook_one
+				rook_two = Rook.new(game, rook_sq_two, 'w')
+				rook_sq_two.occupant = rook_two
+				king = King.new(game, king_square, 'b')
+				king_square.occupant = king
+
+				it "returns true" do 
+					expect(dummy_class.checkmate(king, game)).to eql(true)
+				end
+			end
+
+			context "when the King can move elsewhere out of check" do 
+				game = GameBoard.new
+				rook_sq_one = game.square_at([0, 7])
+				rook_sq_two = game.square_at([1, 6])
+				king_square = game.square_at([7, 6])
+				rook_one = Rook.new(game, rook_sq_one, 'w')
+				rook_sq_one.occupant = rook_one
+				rook_two = Rook.new(game, rook_sq_two, 'w')
+				rook_sq_two.occupant = rook_two
+				king = King.new(game, king_square, 'b')
+				king_square.occupant = king
+
+				it "returns false" do 
+					expect(dummy_class.checkmate(king, game)).to eql(false)
+				end
+			end
+		end
+
+		context "when the King is not in check" do 
+
+			it "returns false" do 
+				game = dummy_class.create
+				king = game.square_at([0, 4]).occupant
+
+				expect(dummy_class.checkmate(king, game)).to eql(false)
 			end
 		end
 	end
