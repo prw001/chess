4945397b98f3663fd64c9eab0c6ed62f7df74d38
Paul Winter <pwinter001@gmail.com>
2018-08-22 22:28:06 -0700

    update .can_be_blocked, add .get_attacking_path

--- lib/GameTools.rb
+++ lib/GameTools.rb
@@ -11,6 +11,17 @@ module GameTools
 		return rows_copy
 	end
 
+	def abs(value)
+		case 
+		when value == 0
+			return 0
+		when value < 0
+			return value * -1
+		else
+			return value
+		end
+	end
+
 	def assemble(game)
 		black_pieces = [
 			Rook.new(game, game.square_at([0, 0]), 'b'),
@@ -70,9 +81,68 @@ module GameTools
 		end
 	end
 
+	def get_attacking_path(attacker, king, game)
+		path = [attacker.position]
+		origin = attacker.position.coordinates.dup
+		coord_diff = difference_of_coordinates(attacker.position.coordinates, king.position.coordinates)
+
+		if coord_diff[0] == 0
+			diff = coord_diff[1]
+			delta = [0, (diff / abs(diff))]
+		elsif coord_diff[1] == 0
+			diff = coord_diff[0]
+			delta = [(diff / abs(diff)), 0]
+		else
+			diff = coord_diff[0]
+			delta = [(diff / abs(diff)), (diff / abs(diff))]
+		end
+
+		(diff - 1).times do 
+			origin = combine_coordinates(origin, delta)
+			path << game.square_at(origin)
+		end
+		return path
+	end
+
+	def can_be_blocked(king, game)
+		attacking_pieces = []
+		if king.color == 'w'
+			opposite_piece_set = game.black_pieces
+			ally_piece_set = game.white_pieces
+		else
+			opposite_piece_set = game.white_pieces
+			ally_piece_set = game.black_pieces
+		end
+
+		opposite_piece_set.each do |piece|
+			if (piece.get_valid_moves.include? king.position)
+				attacking_pieces << piece
+			end
+		end
+		if attacking_pieces.length > 1
+			return false
+		end
+		if attacking_pieces.length == 1
+			unless (attacking_pieces[0].instance_of? Knight)
+				attacking_path = get_attacking_path(attacking_pieces[0], king, game)
+			else
+				attacking_path << attacking_pieces[0].position
+			end
+			ally_piece_set.each do |piece|
+				moves = piece.get_valid_moves
+				moves.each do |move|
+					if (attacking_path.include? move)
+						return true
+					end
+				end
+			end
+		end
+		return false
+	end
+
 	def checkmate(king, game)
 		if is_in_check(king.color, game, king.position) && 
-			(king.get_valid_moves == [])
+			(king.get_valid_moves == []) && !(can_be_blocked(king, game))
 				game.game_over = true
 				return true
 		else
